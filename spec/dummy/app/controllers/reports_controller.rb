ebd6b65ee42f95eeb665c56109a8c2571cc378ac
Dieter S. <101627195+dieter-medium@users.noreply.github.com>
2025-04-29 20:48:59 +0200

    fix: update PDF rendering layout in reports controller
    Add a layout option to the PDF rendering in the reports controller. This change ensures that the generated PDF uses the specified layout for better formatting and presentation.

--- spec/dummy/app/controllers/reports_controller.rb
+++ spec/dummy/app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   def show
     respond_to do |format|
       format.html
-      format.pdf { render pdf: 'my-report' }
+      format.pdf { render pdf: 'my-report', layout: 'pdf' }
     end
   end
 
