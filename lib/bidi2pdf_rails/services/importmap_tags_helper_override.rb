f1c29f0dbce1c01b2fe7527e472cf0c1a8c0265a
Dieter S. <101627195+dieter-medium@users.noreply.github.com>
2025-05-01 07:41:51 +0200

    fix: update importmap tags helper for PDF rendering
    Adds a require statement for the importmap/map to ensure the correct caching key is used when rendering views as PDFs. This change addresses potential issues with asset link management in PDF responses.

--- lib/bidi2pdf_rails/services/importmap_tags_helper_override.rb
+++ lib/bidi2pdf_rails/services/importmap_tags_helper_override.rb
@@ -3,24 +3,22 @@
 module Bidi2pdfRails
   module Services
     module ImportmapTagsHelperOverride
-      def javascript_importmap_tags(*args, **opts, &block)
-        def javascript_importmap_tags(entry_point = "application", importmap: Rails.application.importmap)
-          safe_join [
-                      javascript_inline_importmap_tag(importmap.to_json(resolver: self, cache_key: cache_key)),
-                      javascript_importmap_module_preload_tags(importmap, entry_point:),
-                      javascript_import_module_tag(entry_point)
-                    ], "\n"
-        end
+      def javascript_importmap_tags(entry_point = "application", importmap: Rails.application.importmap)
+        safe_join [
+                    javascript_inline_importmap_tag(importmap.to_json(resolver: self, cache_key: cache_key)),
+                    javascript_importmap_module_preload_tags(importmap, entry_point:),
+                    javascript_import_module_tag(entry_point)
+                  ], "\n"
+      end
 
-        def cache_key
-          asset_host = self.controller.asset_host.respond_to?(:call) ? self.controller.asset_host.call(nil, self.controller.request) : self.controller.asset_host
+      def cache_key
+        asset_host = self.controller.asset_host.respond_to?(:call) ? self.controller.asset_host.call(nil, self.controller.request) : self.controller.asset_host
 
-          asset_host || :json
-        end
+        asset_host || :json
+      end
 
-        def javascript_importmap_module_preload_tags(importmap = Rails.application.importmap, entry_point: "application")
-          javascript_module_preload_tag(*importmap.preloaded_module_paths(resolver: self, entry_point:, cache_key: "#{entry_point}-#{cache_key}"))
-        end
+      def javascript_importmap_module_preload_tags(importmap = Rails.application.importmap, entry_point: "application")
+        javascript_module_preload_tag(*importmap.preloaded_module_paths(resolver: self, entry_point:, cache_key: "#{entry_point}-#{cache_key}"))
       end
     end
   end
